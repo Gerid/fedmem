daddf8f8a6b5afd1dec0ce2210de6eee4b0d08a1
Gerid <915826939@qq.com>
2026-03-19 18:06:42 +0800

    Add factorized adapter model

--- fedprotrack/models/__init__.py
+++ fedprotrack/models/__init__.py
@@ -3,6 +3,11 @@ from __future__ import annotations
 """PyTorch-based models for federated learning on GPU."""
 
 from .torch_feature_adapter import TorchFeatureAdapterClassifier
+from .torch_factorized_adapter import TorchFactorizedAdapterClassifier
 from .torch_linear import TorchLinearClassifier
 
-__all__ = ["TorchLinearClassifier", "TorchFeatureAdapterClassifier"]
+__all__ = [
+    "TorchLinearClassifier",
+    "TorchFeatureAdapterClassifier",
+    "TorchFactorizedAdapterClassifier",
+]
